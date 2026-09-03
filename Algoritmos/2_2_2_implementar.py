42aee481712f9ac90db198dd5d68f5d02e044189
JDavid-Moreno <davidmorenodaleman@gmail.com>
2026-09-03 14:09:22 -0500

    terminado 2.2.2

--- Algoritmos/2_2_2_implementar.py
+++ Algoritmos/2_2_2_implementar.py
@@ -63,32 +63,79 @@ class TreeGame(Game):
 
     def depth(self, state):
         queue = deque()
-        level = 0
-        queue.append((self.initial_state, level))
+        queue.append((self.initial_state, 0))
         while queue:
             node, node_depth = queue.popleft()
             if node == state:
                 return node_depth
-            if state.is_terminal:
+            if self.is_terminal(node):
                 continue
-            level += 1
-            for successor in node.values():
-                queue.append(successor, level)
+            for successor in self.nodes[node]:
+                queue.append((successor, node_depth + 1))
+        return -1
 
     def player(self, state):
-        state =
+        if self.depth(state) % 2 == 0:
+            return self.players[0]
+        else:
+            return self.players[1]
 
+    def opponent(self, player):
+        if player == self.players[0]:
+            return self.players[1]
+        else:
+            return self.players[0]
+
+    def result_actions(self, state):
+        if self.is_terminal(state):
+            return set()
+        else:
+            return set(self.nodes[state])
+
+    def utility(self, state, player):
+        return float(state)
 
 
 def play_game(game, player):
-    state = game.initial
+    state = game.initial_state
     steps = [state]
 
     while not game.is_terminal(state):
-        pass # IMPLEMENTE
+        shift = game.player(state)
+        best_successor = None
+        best_value = float('-inf') if shift == game.players[0] else float('inf')
+        for successor in game.result_actions(state):
+            value = MaxMinSearch.minmax_search(game, successor)
+            if (shift == game.players[0]  and value > best_value) or (shift == game.players[1]  and value < best_value):
+                best_value = value
+                best_successor = successor
+
+        state = best_successor
+        steps.append(state)
 
     return steps
 
+nodes = {
+    "A": ["B", "C", "D"],
+    "B": ["E", "F", "G"],
+    "C": ["H", "I", "J"],
+    "D": ["K", "L", "M"],
+    "E": [8, 7, 2],
+    "F": [9, 1, 6],
+    "G": [2, 4, 1],
+    "H": [1, 3, 5],
+    "I": [3, 9, 2],
+    "J": [6, 5, 2],
+    "K": [1, 2, 3],
+    "L": [9, 7, 2],
+    "M": [16, 6, 4],
+}
 
+game = TreeGame(
+    initial_state = "A",
+    players = ("J1", "J2"),
+    nodes = nodes
+)
 
+print("Secuencia de play_game:", play_game(game, "J1"))
 
